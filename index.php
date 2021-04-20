7ffc885b4b2b806af241660a1991bf1758698e38
Bertrand Bouland <bbformationcefim@gmail.com>
2021-04-20 21:04:17 +0200

    reste hover sur homepage

--- index.php
+++ index.php
@@ -45,7 +45,25 @@
                         <div class="card">
                             <img src="img\code-screen.jpg" srcset="img\code-screen-2x.jpg 2x" alt="code sur un écran">
                             <div class="card-content">
-                                <p class="date">12/02/2022</p>
+                                <div class="date-row">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17.667" viewBox="0 0 16 17.667">
+                                    <defs>
+                                        <style>
+                                            .a {
+                                                fill: none;
+                                                stroke: #663014;
+                                                stroke-linecap: round;
+                                                stroke-linejoin: round;
+                                            }
+                                        </style>
+                                    </defs>
+                                    <g transform="translate(0.5 0.5)">
+                                        <path class="a" d="M4.167,3.333H15.833A1.667,1.667,0,0,1,17.5,5V16.667a1.667,1.667,0,0,1-1.667,1.667H4.167A1.667,1.667,0,0,1,2.5,16.667V5A1.667,1.667,0,0,1,4.167,3.333Z" transform="translate(-2.5 -1.667)" />
+                                        <path class="a" d="M13.333,1.667V5" transform="translate(-2.5 -1.667)" />
+                                        <path class="a" d="M6.667,1.667V5" transform="translate(-2.5 -1.667)" />
+                                        <path class="a" d="M2.5,8.333h15" transform="translate(-2.5 -1.667)" />
+                                    </g>
+                                </svg><p class="date">12/02/2022</p></div>
                                 <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</h3>
                                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
                                     tempor
@@ -56,8 +74,6 @@
                                         <defs>
                                             <style>
                                                 .a {
-                                                    fill: none;
-                                                    stroke: #fff;
                                                     stroke-linecap: round;
                                                     stroke-linejoin: round;
                                                     stroke-width: 2px;
@@ -65,9 +81,9 @@
                                             </style>
                                         </defs>
                                         <g transform="translate(1 1)">
-                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)" />
-                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)" />
-                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)" />
+                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)"></path>
                                         </g>
                                     </svg>
                                 </div>
@@ -78,7 +94,25 @@
                         <div class="card">
                             <img src="img\macbook-table.jpg" srcset="img\macbook-table-2x.jpg 2x" alt="macbook sur une table">
                             <div class="card-content">
-                                <p class="date">12/02/2022</p>
+                                <div class="date-row">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17.667" viewBox="0 0 16 17.667">
+                                    <defs>
+                                        <style>
+                                            .a {
+                                                fill: none;
+                                                stroke: #663014;
+                                                stroke-linecap: round;
+                                                stroke-linejoin: round;
+                                            }
+                                        </style>
+                                    </defs>
+                                    <g transform="translate(0.5 0.5)">
+                                        <path class="a" d="M4.167,3.333H15.833A1.667,1.667,0,0,1,17.5,5V16.667a1.667,1.667,0,0,1-1.667,1.667H4.167A1.667,1.667,0,0,1,2.5,16.667V5A1.667,1.667,0,0,1,4.167,3.333Z" transform="translate(-2.5 -1.667)" />
+                                        <path class="a" d="M13.333,1.667V5" transform="translate(-2.5 -1.667)" />
+                                        <path class="a" d="M6.667,1.667V5" transform="translate(-2.5 -1.667)" />
+                                        <path class="a" d="M2.5,8.333h15" transform="translate(-2.5 -1.667)" />
+                                    </g>
+                                </svg><p class="date">12/02/2022</p></div>
                                 <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</h3>
                                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
                                     tempor
@@ -89,8 +123,6 @@
                                         <defs>
                                             <style>
                                                 .a {
-                                                    fill: none;
-                                                    stroke: #fff;
                                                     stroke-linecap: round;
                                                     stroke-linejoin: round;
                                                     stroke-width: 2px;
@@ -98,9 +130,9 @@
                                             </style>
                                         </defs>
                                         <g transform="translate(1 1)">
-                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)" />
-                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)" />
-                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)" />
+                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)"></path>
                                         </g>
                                     </svg>
                                 </div>
@@ -111,7 +143,25 @@
                         <div class="card">
                             <img src="img\keyboard.jpg" srcset="img\keyboard-2x.jpg 2x" alt="clavier rétroéclairé">
                             <div class="card-content">
-                                <p class="date">12/02/2022</p>
+                                <div class="date-row">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17.667" viewBox="0 0 16 17.667">
+                                    <defs>
+                                        <style>
+                                            .a {
+                                                fill: none;
+                                                stroke: #663014;
+                                                stroke-linecap: round;
+                                                stroke-linejoin: round;
+                                            }
+                                        </style>
+                                    </defs>
+                                    <g transform="translate(0.5 0.5)">
+                                        <path class="a" d="M4.167,3.333H15.833A1.667,1.667,0,0,1,17.5,5V16.667a1.667,1.667,0,0,1-1.667,1.667H4.167A1.667,1.667,0,0,1,2.5,16.667V5A1.667,1.667,0,0,1,4.167,3.333Z" transform="translate(-2.5 -1.667)" />
+                                        <path class="a" d="M13.333,1.667V5" transform="translate(-2.5 -1.667)" />
+                                        <path class="a" d="M6.667,1.667V5" transform="translate(-2.5 -1.667)" />
+                                        <path class="a" d="M2.5,8.333h15" transform="translate(-2.5 -1.667)" />
+                                    </g>
+                                </svg><p class="date">12/02/2022</p></div>
                                 <h3>Lorem ipsum dolor sit amet, consetetur sadipscing elitr</h3>
                                 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
                                     tempor
@@ -122,8 +172,6 @@
                                         <defs>
                                             <style>
                                                 .a {
-                                                    fill: none;
-                                                    stroke: #fff;
                                                     stroke-linecap: round;
                                                     stroke-linejoin: round;
                                                     stroke-width: 2px;
@@ -131,9 +179,9 @@
                                             </style>
                                         </defs>
                                         <g transform="translate(1 1)">
-                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)" />
-                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)" />
-                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)" />
+                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)"></path>
                                         </g>
                                     </svg>
                                 </div>
@@ -213,8 +261,6 @@
                                         <defs>
                                             <style>
                                                 .a {
-                                                    fill: none;
-                                                    stroke: #fff;
                                                     stroke-linecap: round;
                                                     stroke-linejoin: round;
                                                     stroke-width: 2px;
@@ -222,9 +268,9 @@
                                             </style>
                                         </defs>
                                         <g transform="translate(1 1)">
-                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)" />
-                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)" />
-                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)" />
+                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)"></path>
                                         </g>
                                     </svg>
                                 </div>
@@ -246,8 +292,6 @@
                                         <defs>
                                             <style>
                                                 .a {
-                                                    fill: none;
-                                                    stroke: #fff;
                                                     stroke-linecap: round;
                                                     stroke-linejoin: round;
                                                     stroke-width: 2px;
@@ -255,9 +299,9 @@
                                             </style>
                                         </defs>
                                         <g transform="translate(1 1)">
-                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)" />
-                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)" />
-                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)" />
+                                            <path class="a" d="M22,12A10,10,0,1,1,12,2,10,10,0,0,1,22,12Z" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M12,16l4-4L12,8" transform="translate(-2 -2)"></path>
+                                            <path class="a" d="M8,12h8" transform="translate(-2 -2)"></path>
                                         </g>
                                     </svg>
                                 </div>
